fdb323e91cdcd5a7e1232ebbc78d2babf3d9dc82
R6Doc <fiendgear@gmail.com>
2020-09-17 20:44:29 -0500

    added files

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Use Reddid api to get subs"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """returns the number of subs of a subredit"""
+    if subreddit is None or type(subreddit) is not str:
+        return 0
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': 'Python/requests:APIproject:\
+v1.0.0 (by /u/aaorrico23)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
